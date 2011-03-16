3deca6b6e5c44cab6433747b47130c2e6ecb8378
Eric Gjertsen <ericgj72@gmail.com>
2011-03-16 00:04:41 -0400

    remove config files from git

--- config/listener.rb
+++
@@ -1,7 +0,0 @@
-config.environment = :test
-config.server = "127.0.0.1"
-config.port = 6667
-config.nick = "irb___"
-config.realname = "irb___"
-config.channels = ['#test']
-
--- config/supervisor.rb
+++
@@ -1,7 +0,0 @@
-config.environment = :test
-config.server = "127.0.0.1"
-config.port = 6667
-config.nick = "irbbot"
-config.realname = "irbbot"
-config.channels = ['#test']
-
